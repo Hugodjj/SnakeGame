352aa0c5025fe12d6dfbafdf6a140b94f55eb8f9
Hugo Nascimento <hugons@dcc.ufrj.br>
2022-01-05 12:55:13 -0300

    adicionando a cobrinha na tela e outras mudanças

--- snake.py
+++ snake.py
@@ -1,15 +1,27 @@
 import pygame
 from pygame.locals import *
 
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
+
 # Lógica para a criação da janela do jogo.
 
 pygame.init()
-game_screen = pygame.display.set_mode((600, 600))
+game_screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Snake')
+
+# Criação da cobrinha
+
+snake = [(300, WINDOW_HEIGHT/2), (310, WINDOW_HEIGHT/2), (320, WINDOW_HEIGHT/2)]
+skin_snake = pygame.Surface((10, 10))
+skin_snake.fill((255, 255, 255)) # Branco RGB
 
 while True:
 
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
+    game_screen.fill((0, 128, 0)) # Cor do campo Verde RGB
+    for pos in snake:
+        game_screen.blit(skin_snake, pos)
     pygame.display.update()
